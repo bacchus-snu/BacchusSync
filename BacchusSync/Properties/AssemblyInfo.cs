874c573419fe6ed023024765c8a90edfa9572cd3
YeonGyu Jeong <81887821@hanmail.net>
2019-03-19 13:26:06 +0900

    Added AssemblyInfo.cs, and updated gitignore.

---
+++ BacchusSync/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BacchusSync")]
+[assembly: AssemblyDescription("Bacchus user profile sync plugin")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bacchus")]
+[assembly: AssemblyProduct("BacchusSync")]
+[assembly: AssemblyCopyright("MIT")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a56d528b-c55e-4553-8a73-4a7aa2e3850c")]
+
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
---
+++ Updater/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bacchus Sync Updater")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bacchus")]
+[assembly: AssemblyProduct("Bacchus Sync Updater")]
+[assembly: AssemblyCopyright("MIT")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("80088b3d-419d-42e1-a9aa-4d2f0befe4e8")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
